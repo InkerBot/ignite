a0ed75f61c2220d74c74fd64cfb32632529ba44b
InkerBot <im@inker.bot>
2023-12-21 00:56:51 +0800

    maven repository ends with '/'

--- .space.kts
+++ .space.kts
@@ -1,5 +1,5 @@
 job("Build and publish") {
     gradlew("mcr.microsoft.com/openjdk/jdk:17-ubuntu", "build", "publish") {
-        env["JB_SPACE_MAVEN_REPOSITORY"] = "https://packages.inksnow.org/maven/p/main/maven"
+        env["JB_SPACE_MAVEN_REPOSITORY"] = "https://packages.inksnow.org/maven/p/main/maven/"
     }
 }
